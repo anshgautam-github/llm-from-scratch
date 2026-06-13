190f2f884b346fa91609a1bd854008d309bfa8c6
Ansh Gautam <anshgautam1011@gmail.com>
2026-06-13 11:54:21 +0530

    Dataset Loaded

--- 01-LLM_Data_Preprocessing/01-Creating_Tokens_to_TokenID.py
+++ 01-LLM_Data_Preprocessing/01-Creating_Tokens_to_TokenID.py
@@ -9,12 +9,19 @@ preprocessed = [item.strip() for item in preprocessed if item.strip()]
 print(preprocessed[:10])
 print(len(preprocessed))
 
+print("\n Now we will apply the same logic to a larger dataset. We will read the contents of a text file and preprocess it to create tokens. The file the-verdict.txt is assumed to contain the text data we want to process.\n")
 
 #Applying the same logic to a larger data₹set, we will read the contents of a text file and preprocess it to create tokens. The file "the-verdict.txt" is assumed to contain the text data we want to process.:
-with open("the-verdict.txt", "r", encoding="utf-8") as f:
+with open("../the-verdict.txt", "r", encoding="utf-8") as f:
     raw_text = f.read() # It returns the entire contents of the file as a single string.
 
 print("Total number of character:", len(raw_text))
 print(raw_text[:99])
 print(type(raw_text))
 
+print("\n\nPreprocessing the text to create tokens...")
+
+preprocessed = re.split(r'([,.:;?_!"()\']|--|\s)', raw_text)
+preprocessed = [item.strip() for item in preprocessed if item.strip()]
+print(preprocessed[:30])
+print(len(preprocessed))
