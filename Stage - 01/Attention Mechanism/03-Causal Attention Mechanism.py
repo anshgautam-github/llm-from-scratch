c24c2f4dc051572c2aed7aa84c9385b5a6db2848
Ansh Gautam <anshgautam1011@gmail.com>
2026-06-20 03:58:32 +0530

    Causal Attention Mechanism

--- Stage - 01/Attention Mechanism/03-Causal Attention Mechanism.py
+++ Stage - 01/Attention Mechanism/03-Causal Attention Mechanism.py
@@ -59,3 +59,48 @@ print(masked)
 # Now, all we need to do is apply the softmax function to these masked results, and we are done.
 attn_weights = torch.softmax(masked / keys.shape[-1]**0.5, dim=1)
 print(attn_weights)
+
+
+
+# The following CausalAttention class is similar to the SelfAttention class we implemented earlier, except 
+# that we now added the dropout and causal mask components as highlighted in the following code.
+
+torch.manual_seed(123)
+dropout = torch.nn.Dropout(0.5) #A
+example = torch.ones(6, 6) #B
+print(dropout(example))
+
+class CausalAttention(nn.Module):
+
+    def __init__(self, d_in, d_out, context_length,
+                 dropout, qkv_bias=False):
+        super().__init__()
+        self.d_out = d_out
+        self.W_query = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_key   = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_value = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.dropout = nn.Dropout(dropout) # New
+        self.register_buffer('mask', torch.triu(torch.ones(context_length, context_length), diagonal=1)) # New
+
+    def forward(self, x):
+        b, num_tokens, d_in = x.shape # New batch dimension b
+        keys = self.W_key(x)
+        queries = self.W_query(x)
+        values = self.W_value(x)
+
+        attn_scores = queries @ keys.transpose(1, 2) # Changed transpose
+        attn_scores.masked_fill_(  # New, _ ops are in-place
+            self.mask.bool()[:num_tokens, :num_tokens], -torch.inf)  # `:num_tokens` to account for cases where the number of tokens in the batch is smaller than the supported context_size
+        attn_weights = torch.softmax(
+            attn_scores / keys.shape[-1]**0.5, dim=-1
+        )
+        attn_weights = self.dropout(attn_weights) # New
+
+        context_vec = attn_weights @ values
+        return context_vec
+    
+torch.manual_seed(123)
+context_length = batch.shape[1]
+ca = CausalAttention(d_in, d_out, context_length, 0.0)
+context_vecs = ca(batch)
+print("context_vecs.shape:", context_vecs.shape)
