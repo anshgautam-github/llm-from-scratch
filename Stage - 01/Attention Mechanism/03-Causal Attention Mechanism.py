5829f71636f805664930d29dbdd26f3ef4434e8b
Ansh Gautam <anshgautam1011@gmail.com>
2026-06-20 03:55:13 +0530

    CAM

--- Stage - 01/Attention Mechanism/03-Causal Attention Mechanism.py
+++ Stage - 01/Attention Mechanism/03-Causal Attention Mechanism.py
@@ -1,3 +1,17 @@
+import torch
+
+# Sppose we have embedding vector for each of them
+
+inputs = torch.tensor(
+  [[0.43, 0.15, 0.89], # Your     (x^1)
+   [0.55, 0.87, 0.66], # journey  (x^2)
+   [0.57, 0.85, 0.64], # starts   (x^3)
+   [0.22, 0.58, 0.33], # with     (x^4)
+   [0.77, 0.25, 0.10], # one      (x^5)
+   [0.05, 0.80, 0.55]] # step     (x^6)
+)
+d_in = inputs.shape[1] #B
+d_out = 2 #C
 
 # Reuse the query and key weight matrices of the SelfAttention_v2 object from the previous section for convenience
 class SelfAttention_v2(nn.Module):
