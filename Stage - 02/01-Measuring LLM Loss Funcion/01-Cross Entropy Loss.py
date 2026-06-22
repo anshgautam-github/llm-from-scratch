cc8b32c6888f3c79126e29e780bc2b984918f023
Ansh Gautam <anshgautam1011@gmail.com>
2026-06-22 17:06:12 +0530

    Cross Entropy manual

--- Stage - 02/01-Measuring LLM Loss Funcion/01-Cross Entropy Loss.py
+++ Stage - 02/01-Measuring LLM Loss Funcion/01-Cross Entropy Loss.py
@@ -5,3 +5,31 @@ inputs = torch.tensor([[16833, 3626, 6100],   # ["every effort moves",
 
 targets = torch.tensor([[3626, 6100, 345  ],  # [" effort moves you",
                         [1107,  588, 11311]]) #  " really like chocolate"]
+
+with torch.no_grad():
+    logits = model(inputs) #got the logits after output head
+
+probas = torch.softmax(logits, dim=-1) # Probability of each token in vocabulary
+
+token_ids = torch.argmax(probas, dim=-1, keepdim=True)
+print(f"Targets batch 1: {token_ids_to_text(targets[0], tokenizer)}")
+print(f"Outputs batch 1: {token_ids_to_text(token_ids[0].flatten(), tokenizer)}")
+
+# Here we are finding the probability of the current token that we have currently without training
+text_idx = 0
+target_probas_1 = probas[text_idx, [0, 1, 2], targets[text_idx]]
+print("Text 1:", target_probas_1)
+text_idx = 1
+target_probas_2 = probas[text_idx, [0, 1, 2], targets[text_idx]]
+print("Text 2:", target_probas_2)
+
+# Compute logarithm of all token probabilities
+log_probas = torch.log(torch.cat((target_probas_1, target_probas_2)))
+print(log_probas)
+
+# Calculate the average probability for each token
+avg_log_probas = torch.mean(log_probas)
+print(avg_log_probas)
+
+neg_avg_log_probas = avg_log_probas * -1
+print(neg_avg_log_probas)
