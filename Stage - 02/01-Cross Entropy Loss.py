83ad07d8b06b7a9530c9ce63f5ec9762668ce5c0
Ansh Gautam <anshgautam1011@gmail.com>
2026-06-22 17:02:44 +0530

    Cross entropy

---
+++ Stage - 02/01-Cross Entropy Loss.py
@@ -0,0 +1,7 @@
+# We see first for these two input. Then we will apply this concept for the whole data set of the book.
+
+inputs = torch.tensor([[16833, 3626, 6100],   # ["every effort moves",
+                       [40,    1107, 588]])   #  "I really like"]
+
+targets = torch.tensor([[3626, 6100, 345  ],  # [" effort moves you",
+                        [1107,  588, 11311]]) #  " really like chocolate"]
