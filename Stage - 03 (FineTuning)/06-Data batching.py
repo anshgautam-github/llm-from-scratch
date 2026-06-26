55bf0e7d9b3604018473b9c987cbd1bf3a939af7
Ansh Gautam <anshgautam1011@gmail.com>
2026-06-26 11:09:19 +0530

    Batching

--- Stage - 03 (FineTuning)/06-Data batching.py
+++ Stage - 03 (FineTuning)/06-Data batching.py
@@ -209,3 +209,56 @@ print(targets)
 # tensor([[    1,     2,     3,     4, 50256],
 #         [    6, 50256,  -100,  -100,  -100],
 #         [    8,     9, 50256,  -100,  -100]])
+
+
+
+# The modified collate function works as expected, altering the target list by inserting the token ID -100.
+# What is the logic behind this adjustment? Let's explore the underlying purpose of this modification.
+# For demonstration purposes, consider the following simple and self-contained example where each output logit can correspond to a potential token from the model's vocabulary.
+
+# Here's how we might calculate the cross entropy loss (introduced in chapter 5) during training when the model predicts a sequence of tokens, 
+logits_1 = torch.tensor(
+    [[-1.0, 1.0],  # 1st training example
+     [-0.5, 1.5]]  # 2nd training example
+)
+targets_1 = torch.tensor([0, 1])
+loss_1 = torch.nn.functional.cross_entropy(logits_1, targets_1)
+print(loss_1)   #tensor(1.1269)
+
+# Adding an additional token ID will, as we would expect, affect the loss calculation.
+logits_2 = torch.tensor(
+    [[-1.0, 1.0],
+     [-0.5, 1.5],
+     [-0.5, 1.5]]  # New 3rd training example
+)
+targets_2 = torch.tensor([0, 1, 1])
+loss_2 = torch.nn.functional.cross_entropy(logits_2, targets_2)
+print(loss_2)   #tensor(0.7936)
+
+# Now, let's get to the interesting part and see what happens if we replace the third target token ID with -100:
+targets_3 = torch.tensor([0, 1, -100])
+loss_3 = torch.nn.functional.cross_entropy(logits_2, targets_3)
+print(loss_3)
+print("loss_1 == loss_3:", loss_1 == loss_3)
+# tensor(1.1269)
+# loss_1 == loss_3: tensor(True)
+
+
+# Based on this result, we can see that the resulting loss on these 3 training examples is identical to the loss we calculated from the 2 training examples earlier.
+# In other words, the cross entropy loss function ignored the third entry in the targets_3 vector, the token ID corresponding to -100.
+# (Interested readers can try to replace the -100 value with another token IDs that is not 0 or 1, and will see that this results in an error.)
+# So, what's so special about -100 that it's ignored by the cross entropy loss? The default setting of the cross entropy function in PyTorch is cross_entropy(..., ignore_index=-100).
+
+# This means that it ignores targets labeled with -100.
+# In this, we take advantage of this ignore_index to ignore the additional end-oftext (padding) tokens that we used to pad the training examples to have the same length in each batch.
+# However, we want to keep one 50256 (end-of-text) token ID in the targets because it helps the LLM to learn to generate end-of-text tokens, which we can use as an indicator that a response is complete.
+
+# MASKING TARGET TOKEN IDS
+# In addition to masking out padding tokens, it is also common to mask out the target token IDs that correspond to the instruction
+# By masking out the target token IDs that correspond to the instruction, the LLM cross entropy loss is only computed for the generated response target IDs.
+# By masking out the instruction tokens, the model is trained to focus on generating accurate responses rather than additionally also memorizing instructions, which can help with reducing overfitting.
+
+# Currently, researchers are divided on whether masking the instructions is universally beneficial during instruction finetuning.
+# For instance, a recent paper titled "Instruction Tuning With Loss Over Instructions" demonstrated that not masking the instructions benefits the LLM performance.
+# In this, we do not apply masking and leave it as an optional exercise for the reader.
+
